85ff606348e8ab9be718a2c55764263eb78055c9
S0018211704 <prakash.kamaraj@de.ey.com>
2018-07-17 06:43:58 +0000

    Added Dynamic Table

--- webapp/controller/TableViewer.controller.js
+++ webapp/controller/TableViewer.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/table/Column"
+], function (Controller, tableColumn) {
 	"use strict";
 
 	return Controller.extend("pinaki.ey.tableviewer.controller.TableViewer", {
@@ -13,12 +14,38 @@ sap.ui.define([
 			this.queryData(tableName);
 		},
 		queryData: function (tableName) {
+			var that = this;
 			$.ajax({
 				url: "/eyhcp/Pinaki/RandomDataGenerator/Scripts/tableDisplay.xsjs?tableName=" + tableName,
 				cache: false,
 				success: function (data) {
-
+					that.getView().getModel().setData({
+						tableData: data,
+						tableName: tableName
+					}, true);
+					that.processData().bind(that);
 				}
+			});
+		},
+		processData: function () {
+			var oModel = this.getView().getModel();
+			var tableData = oModel.getData().tableData;
+			var objKeys = [];
+			for (var k in tableData[0]) objKeys.push(k);
+			this.createTableColumns(objKeys);
+		},
+		createTableColumns: function (objKeys) {
+			var table = this.getView().byId('idDataTable');
+			objKeys.forEach(function (e) {
+				var column = new tableColumn({
+					label: new sap.m.Label({
+						text: e
+					}),
+					template: new sap.m.Input({
+						value: "{" + e + "}"
+					})
+				});
+				table.addColumn(column);
 			});
 		}
 
